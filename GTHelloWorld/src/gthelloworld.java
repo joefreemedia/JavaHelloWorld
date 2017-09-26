849649e5047c4324c4aac543fbd394d55c7ff37a
gtrenard <gtrenard@USSTDT-GTRE.internal.corp>
2017-09-26 16:47:02 -0400

    Second commit, we added a second sentence

--- GTHelloWorld/src/gthelloworld.java
+++ GTHelloWorld/src/gthelloworld.java
@@ -4,6 +4,7 @@ public class gthelloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Does this go to github?");
+		System.out.print(" Yes we got it on the github repository!");
 	}
 
 }
